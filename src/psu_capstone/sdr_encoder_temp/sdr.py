66b627f6d86ac313bcd631ba3beb35399a63305f
millscb <cbm6050@psu.edu>
2025-11-11 12:17:59 -0600

    QA branch (static testing) - fixed place holder sdr empty method -
    python:S1186

--- src/psu_capstone/sdr_encoder_temp/sdr.py
+++ src/psu_capstone/sdr_encoder_temp/sdr.py
@@ -1,7 +1,20 @@
-from typing import List
+"""Sparse Distributed Representation (SDR) class implementation fist pass
+The new Sdr.py is in another branch. This is just a placeholder for now."""
+
+from typing import Callable, List, Optional
 
 
 class SDR:
+
+    elem_dense = int  #: Dense element type used for storing SDR bits.
+    elem_sparse = int  #: Sparse index type mirroring the C++ implementation.
+    sdr_dense_t = List[elem_dense]  #: Alias for the dense SDR container type.
+    sdr_sparse_t = List[elem_sparse]  #: Alias for the sparse SDR container type.
+    sdr_coordinate_t = List[
+        List[int]
+    ]  #: Alias representing coordinates grouped per dimension.
+    sdr_callback_t = Callable[[], None]  #: Callback signature invoked on SDR state changes.
+    
     def __init__(self, dimensions: list[int]):
         self.size = 1
         self.dimensions_ = dimensions
@@ -15,6 +28,8 @@ class SDR:
         self.dense_valid = False
         self.sparse_invalid = False
         self.coordinates_valid = False
+        self.__callbacks: List[Optional[sdr_callback_t]] = []
+        self.__destroy_callbacks: List[Optional[sdr_callback_t]] = []
 
     def zero(self):
         self.sparse = []
@@ -42,8 +57,12 @@ class SDR:
         self.sparse_valid = False
         self.coordinates_valid = False
 
-    def do_callbacks(self):
-        pass
+    def do_callbacks(self) -> None:
+        """Notify registered watchers that the SDR value has changed."""
+
+        for callback in self.__callbacks:
+            if callback is not None:
+                callback()
 
     def get_dense(self):
         return self.dense
