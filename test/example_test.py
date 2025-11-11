b2d1d8831aa3d55958b51eeae78ef494bae08185
millscb <cbm6050@psu.edu>
2025-11-11 13:02:38 -0600

    added test pass and test fail

--- test/example_test.py
+++ test/example_test.py
@@ -1,6 +1,11 @@
 """Example test file. for pytest discovery."""
 
 
-def test_example():
+def test_pass():
     """An example test that always passes."""
     assert True
+
+
+def test_fail():
+    """An example test that always fails."""
+    assert False
