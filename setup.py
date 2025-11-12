bb1c4034451434806a41969fa47b0e267cc53032
millscb <cbm6050@psu.edu>
2025-11-12 12:13:43 -0600

    added setup.py for pathing

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup script for psu_capstone package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="psu_capstone",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
